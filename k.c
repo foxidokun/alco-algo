7bcad58248059f4b53aafba0fe60272fdd75e4a2
foxidokun <fox@forestfox.tk>
2023-03-09 21:15:02 +0300

    clang-format

--- k.c
+++ k.c
@@ -1,8 +1,8 @@
+#include <emmintrin.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-#include <emmintrin.h>
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Const & Define
@@ -13,18 +13,19 @@
 #ifdef DEBUG
 #define memory_alloc(count, size) calloc(count, size)
 #else
-#define memory_alloc(count, size) malloc(count*size)
+#define memory_alloc(count, size) malloc(count *size)
 #endif
 
 typedef unsigned int uint;
 
 typedef int (*comparator_f)(const void *, const void *);
 
-#define panic_if_not(action, expected_res) { \
-    if ((action) != expected_res) {          \
-        return -1;                           \
-    }                                        \
-}
+#define panic_if_not(action, expected_res)                                     \
+    {                                                                          \
+        if ((action) != expected_res) {                                        \
+            return -1;                                                         \
+        }                                                                      \
+    }
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Struct Definition
@@ -34,7 +35,8 @@ typedef int (*comparator_f)(const void *, const void *);
 // Prototypes
 // ---------------------------------------------------------------------------------------------------------------------
 
-static void cust_qsort(void *base, size_t count, size_t size, comparator_f comp);
+static void cust_qsort(void *base, size_t count, size_t size,
+                       comparator_f comp);
 
 static int intcmp(const void *lhs, const void *rhs);
 
@@ -52,10 +54,10 @@ int main(void) {
         panic_if_not(scanf("%i", array + i), 1);
     }
 
-    cust_qsort (array, n, sizeof (int), intcmp);
+    cust_qsort(array, n, sizeof(int), intcmp);
 
     for (uint i = 0; i < n; ++i) {
-        printf ("%i ", array[i]);
+        printf("%i ", array[i]);
     }
 
     free(array);
@@ -65,13 +67,12 @@ int main(void) {
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
-
 #define _big_t uint32_t
 #define _tmp_init_val 0
 
 static void swap(void *__restrict a, void *__restrict b, size_t size) {
-    _big_t *c_a_big = (_big_t *) a;
-    _big_t *c_b_big = (_big_t *) b;
+    _big_t *c_a_big = (_big_t *)a;
+    _big_t *c_b_big = (_big_t *)b;
     _big_t tmp_big = _tmp_init_val;
 
     while (size >= sizeof(_big_t)) {
@@ -84,8 +85,8 @@ static void swap(void *__restrict a, void *__restrict b, size_t size) {
         size -= sizeof(_big_t);
     }
 
-    char *c_a = (char *) c_a_big;
-    char *c_b = (char *) c_b_big;
+    char *c_a = (char *)c_a_big;
+    char *c_b = (char *)c_b_big;
     char tmp = 0;
 
     while (size > 0) {
@@ -97,15 +98,16 @@ static void swap(void *__restrict a, void *__restrict b, size_t size) {
         c_b++;
         size--;
     }
-
 }
 
-static void cust_qsort(void *base, size_t count, size_t size, comparator_f comp) {
-    char *const base_p = (char *) base;
+static void cust_qsort(void *base, size_t count, size_t size,
+                       comparator_f comp) {
+    char *const base_p = (char *)base;
     // Small array optimisations
 
-    if (count == 1) { return; }
-    else if (count == 2 && comp(base_p, base_p + size) > 0) {
+    if (count == 1) {
+        return;
+    } else if (count == 2 && comp(base_p, base_p + size) > 0) {
         swap(base_p + size, base_p + size, size);
     } else if (count == 3) {
         if (comp(base_p, base_p + size) > 0) {
@@ -128,14 +130,18 @@ static void cust_qsort(void *base, size_t count, size_t size, comparator_f comp)
     size_t pi = count / 2;
 
     while (hi > lo) {
-        while (comp(base_p + lo * size, base_p + pi * size) < 0 && lo < count) lo++;
-        while (comp(base_p + pi * size, base_p + hi * size) < 0 && hi > 0) hi--;
+        while (comp(base_p + lo * size, base_p + pi * size) < 0 && lo < count)
+            lo++;
+        while (comp(base_p + pi * size, base_p + hi * size) < 0 && hi > 0)
+            hi--;
 
         if (lo <= hi) {
             swap(base_p + lo * size, base_p + hi * size, size);
 
-            if (pi == lo) pi = hi;
-            else if (pi == hi) pi = lo;
+            if (pi == lo)
+                pi = hi;
+            else if (pi == hi)
+                pi = lo;
 
             lo++;
             hi--;
@@ -151,10 +157,9 @@ static void cust_qsort(void *base, size_t count, size_t size, comparator_f comp)
     }
 }
 
-
 static int intcmp(const void *lhs, const void *rhs) {
-    int lhs_val = *(const int *) lhs;
-    int rhs_val = *(const int *) rhs;
+    int lhs_val = *(const int *)lhs;
+    int rhs_val = *(const int *)rhs;
 
     if (lhs_val > rhs_val) {
         return 1;
